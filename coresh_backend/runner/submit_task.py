6c0bea0708062ab584a46fd28aa2ba02f3425755
Vladimir Sukhov <vdsukhov@ya.ru>
2024-10-22 17:06:10 -0500

    Changed the submit_task.py to suite the ris cluster

--- coresh_backend/runner/submit_task.py
+++ coresh_backend/runner/submit_task.py
@@ -2,27 +2,63 @@
 import argparse
 import subprocess
 import os
+import json
+import math
 
-GESECAR_PATH = "/nfs/home/geseca/geseca/geseca-r/"
-OUT_PATH = "/mnt/tank/scratch/geseca/geseca-web/query"
+import random
+import time
+import string
 
-SERVER_HOST = 'sphinx'
-SERVER_USER = 'geseca'
+
+def generate_random_folder_name():
+    # Get current timestamp
+    timestamp = time.strftime("%Y%m%d")
+    
+    # Generate a random string of letters and digits
+    random_part = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
+    
+    # Combine timestamp with the random part to create a unique folder name
+    folder_name = f"{timestamp}_{random_part}"
+    
+    return folder_name
+
+# CORESH_R_PATH = "/nfs/home/geseca/geseca/geseca-r/"
+# OUT_PATH = "/mnt/tank/scratch/geseca/geseca-web/query"
+
+
+# SERVER_HOST = 'sphinx'
+# SERVER_USER = 'geseca'
+
+SERVER_HOST = os.getenv("SERVER_HOST")
+SERVER_USER = os.getenv("SERVER_USER")
+CORESH_R_PATH = os.getenv("CORESH_R_PATH")
+OUT_PATH = os.getenv("CORESH_QUERIES")
+
+
+
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--query-path', type=str, required=True, help='Path to query')
     parser.add_argument('--organism', type=str, required=True, help='Type of organism for search')
     parser.add_argument('--sample-size', default=21, help='Sample size for adaptive multilevel splitting approach')
-    parser.add_argument('--batch-size', default=100, help='The batch size for processing the experiments')
+    parser.add_argument('--batch-size', default=500, help='The batch size for processing the experiments')
     args = parser.parse_args()
     return args.query_path, args.organism, args.sample_size, args.batch_size
 
 def create_temp_dir():
+
+    rand_name = generate_random_folder_name()
     ssh_command = [
         'ssh', f'{SERVER_USER}@{SERVER_HOST}',
-        'mktemp', '-d', f'--tmpdir={OUT_PATH}'
+        'mkdir', '-p', f'{OUT_PATH}/{rand_name}'
     ]
+
+    # ssh_command = [
+    #     'ssh', f'{SERVER_USER}@{SERVER_HOST}',
+    #     'mktemp', '-d', f'--tmpdir={OUT_PATH}'
+    # ]
     try:
         p = subprocess.Popen(
             ssh_command,
@@ -34,7 +70,7 @@ def create_temp_dir():
         pout, perr = p.communicate()
 
         assert p.returncode == 0, f'{perr}'
-        return pout.strip()
+        return f'{OUT_PATH}/{rand_name}'
     except Exception as e:
         print(f"Error in create_temp_dir: {e}")
         return None
@@ -42,7 +78,7 @@ def create_temp_dir():
 def create_subfolders(dst):
     ssh_command = [
         'ssh', f'{SERVER_USER}@{SERVER_HOST}',
-        f'cd {dst}; mkdir input; mkdir -p output/chunks/slurmLog'
+        f'cd {dst}; mkdir input; mkdir -p output/chunks/; mkdir -p output/finalTable/; mkdir -p output/log/'
     ]
     try:
         p = subprocess.Popen(
@@ -60,18 +96,20 @@ def create_subfolders(dst):
         return False
 
 def upload_query_json(query_path, dst):
+    
     try:
-        # TODO remove geseca user
-        subprocess.call(['rsync', '-qP', f'{query_path}', f'geseca@sphinx:{dst}/input/query.json'])
+        create_subfolders(dst)
+        subprocess.call(['rsync', '-qP', f'{query_path}', f'{SERVER_USER}@{SERVER_HOST}:{dst}/input/query.json'])
     except Exception as e:
         print(f"Error occured during upload_query_json: {e}")
 
-def run_slurm_job(fldr, batch_size, nbatches, sample_size):
+        
+def submit_job(fldr, batch_size, nbatches, sample_size):
     ssh_command = [
         'ssh', f'{SERVER_USER}@{SERVER_HOST}',
-        f'cd {GESECAR_PATH}; sbatch --array=1-{nbatches} --output={fldr}/output/chunks/slurmLog/arrayJob_%A_%a.out slurm_runner.sh '
-        f'{fldr}/input/query.json {batch_size} {sample_size} {fldr}/output/chunks'
+        f'source ~/.bash_profile; cd {CORESH_R_PATH}; bash submit_job.sh {fldr} {sample_size} {batch_size} {nbatches}'
     ]
+    
     try:
         p = subprocess.Popen(
             ssh_command,
@@ -81,12 +119,9 @@ def run_slurm_job(fldr, batch_size, nbatches, sample_size):
         )
         pout, perr = p.communicate()
         assert p.returncode == 0, f'{perr}'
-
-        return True
     except Exception as e:
-        print(f'Error in run_slurm_job: {e}')
-        return False
-
+        print(f"Error in submit_job: {e}")
+    
 
 
 if __name__ == "__main__":
@@ -94,22 +129,14 @@ if __name__ == "__main__":
     try:
         folder_name = create_temp_dir()
         assert folder_name is not None, "Folder was not created properly"
-        # print(folder_name)
-        assert create_subfolders(f'{folder_name}'), 'Subfolders were not created properly'
 
-        with open(f'./coresh_backend/ext/data/{org}_file_count.txt') as inp_f:
-            nfiles = int(inp_f.readline().strip())
-        nbatches = nfiles // batch_size + 1
-
+        with open('./coresh_backend/ext/data/number_of_files.json') as inp_f:
+            data = json.load(inp_f)
+            nfiles = int(data[org])
+        nbatches = math.ceil(nfiles / batch_size)
         upload_query_json(query_path, folder_name)
-        run_slurm_job(folder_name, batch_size, nbatches, sample_size)
+        submit_job(folder_name, batch_size, nbatches, sample_size)
 
         print(f"ID: {os.path.basename(folder_name)}")
-
-
-        
-
-
-    
     except Exception as e:
         print(f'Error: {e}')
