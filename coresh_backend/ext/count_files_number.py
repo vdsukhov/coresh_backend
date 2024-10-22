0e24d7ab9502855315dd598168c237749d8eb87d
Vladimir Sukhov <vdsukhov@ya.ru>
2024-10-22 11:25:23 -0500

    Changed approach for count number of files

--- coresh_backend/ext/count_files_number.py
+++ coresh_backend/ext/count_files_number.py
@@ -1,39 +1,35 @@
 #!/usr/bin/env python
 import subprocess
+import os
 
-FILES_PATH = "/mnt/geseca/geseca_pca_datasets"
+# FILES_PATH = "/mnt/geseca/geseca_pca_datasets"
 
-SERVER_HOST = 'sphinx'
-SERVER_USER = 'geseca'
+SERVER_HOST = os.getenv("SERVER_HOST")
+SERVER_USER = os.getenv("SERVER_USER")
+CORESH_R_PATH = os.getenv("CORESH_R_PATH")
+
+print(SERVER_USER, SERVER_HOST, CORESH_R_PATH)
 
 if __name__ == "__main__":
 
-    values = {}
+    ssh_command = [
+        "ssh",
+        f"{SERVER_USER}@{SERVER_HOST}",
+        f"cd {CORESH_R_PATH} && bash ./methods/number_of_files.sh"
+    ]
 
-    for org in ('hsa', 'mmu'):
-        ssh_command = [
-            'ssh',
-            f'{SERVER_USER}@{SERVER_HOST}',
-            f'ls {FILES_PATH}/{org} | wc -l'
-        ]
-
-        try:
-            p = subprocess.Popen(
-                ssh_command,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                universal_newlines=True
-            )
-            pout, perr = p.communicate()
-            assert p.returncode == 0, f"Error during subprocess: {perr}"
-            
-            values[org] = pout.strip()
-
-
-        except Exception as e:
-            print(f"Error occured:\n{e}")
+    try:
+        # Execute the SSH command
+        process = subprocess.Popen(
+            ssh_command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True
+        )
+        stdout, stderr = process.communicate()
         
-        with open(f'./ext/data/{org}_file_count.txt', 'w') as out_f:
-            out_f.write(f'{values[org]}\n')
-
-    
+        # Check if the R script ran successfully
+        assert process.returncode == 0, f'{stderr}'
+        
+    except Exception as e:
+        print(f"Error occurred while running R script: {e}")
